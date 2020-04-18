f4b755e6f8d4ee06fe5e2c556a34db2bbb18d09a
jhorne98 <jhorne@ycp.edu>
2020-04-17 22:12:47 -0400

    Added templates for contracts layer

--- create-new-table-infrastructure.sh
+++ create-new-table-infrastructure.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Create templates for
 
 a=$(echo -e $1 | awk '{print tolower($0)}')
 
@@ -41,5 +42,59 @@ echo -e >> I$1Repository.kt
 echo -e "@Repository" >> I$1Repository.kt
 echo -e "interface I$1Repository" >> I$1Repository.kt
 
-# Add to git
+# Add all to git
+git add .
+
+# Create the Contracts layer files
+cd ../..
+cd contracts
+mkdir $a
+cd $a
+
+touch $1Factory.kt
+echo -e "package com.radiotelescope.contracts.$a" >> $1Factory.kt
+echo -e >> $1Factory.kt
+echo -e "import com.radiotelescope.repository.$a.$1" >> $1Factory.kt
+echo -e >> $1Factory.kt
+echo -e "/**" >> $1Factory.kt
+echo -e " * Abstract factory interface with methods for all [$1] operations" >> $1Factory.kt
+echo -e " */" >> $1Factory.kt
+echo -e "interface $1Factory {" >> $1Factory.kt
+echo -e "}" >> $1Factory.kt
+
+touch Base$1Factory.kt
+echo -e "package com.radiotelescope.contracts.$a" >> Base$1Factory.kt
+echo -e >> Base$1Factory.kt
+echo -e "import com.radiotelescope.repository.$a.I$1Repository" >> Base$1Factory.kt
+echo -e "import com.radiotelescope.repository.$a.$1" >> Base$1Factory.kt
+echo -e >> Base$1Factory.kt
+echo -e "/**" >> Base$1Factory.kt
+echo -e " * Base concrete implementation of the [$1Factory] interface" >> Base$1Factory.kt
+echo -e " *" >> Base$1Factory.kt
+echo -e " * @param videoFileRepo the [I$1Repository] interface" >> Base$1Factory.kt
+echo -e " */" >> Base$1Factory.kt
+echo -e "class Base$1Factory(" >> Base$1Factory.kt
+echo -e "\t\tprivate val ${a}Repo: I$1Repository" >> Base$1Factory.kt
+echo -e ") : $1Factory {" >> Base$1Factory.kt
+echo -e "}" >> Base$1Factory.kt
+
+touch User$1Wrapper.kt
+echo -e "package com.radiotelescope.contracts.$a" >> User$1Wrapper.kt
+echo -e >> User$1Wrapper.kt
+echo -e "import com.radiotelescope.security.UserContext" >> User$1Wrapper.kt
+echo -e >> User$1Wrapper.kt
+echo -e "/**" >> User$1Wrapper.kt
+echo -e " * Wrapper that takes a [$1Factory] and is responsible for all" >> User$1Wrapper.kt
+echo -e " * user role validations for the $1 Entity" >> User$1Wrapper.kt
+echo -e " *" >> User$1Wrapper.kt
+echo -e " * @property context the [UserContext] interface" >> User$1Wrapper.kt
+echo -e " * @property factory the [${1}Factory interface" >> User$1Wrapper.kt
+echo -e " */" >> User$1Wrapper.kt
+echo -e "class User$1Wrapper(" >> User$1Wrapper.kt
+echo -e "\t\tprivate val context: UserContext," >> User$1Wrapper.kt
+echo -e "\t\tprivate val factory: ${1}Factory" >> User$1Wrapper.kt
+echo -e ") {" >> User$1Wrapper.kt
+echo -e "}" >> User$1Wrapper.kt
+
+# Add all to git
 git add .

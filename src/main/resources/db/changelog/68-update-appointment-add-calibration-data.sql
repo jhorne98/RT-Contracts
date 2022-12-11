719179c8198e174df39bf36121601deea54fb133
jsnyder17 <jsnyder17@ycp.edu>
2022-12-11 16:29:34 -0500

    Merged latest changes and appointment calibration changes

---
+++ src/main/resources/db/changelog/68-update-appointment-add-calibration-data.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset lbradley1:123
+
+CREATE TABLE appointment_calibration (
+    id INT NOT NULL AUTO_INCREMENT,
+    appointment_id INT NOT NULL,
+    calibration_type BIT NOT NULL,
+    zenith_start_time DATETIME NOT NULL,
+    zenith_end_time DATETIME NOT NULL,
+    tree_start_time DATETIME NOT NULL,
+    tree_end_time DATETIME NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+-- DROP TABLE appointment_calibration;
